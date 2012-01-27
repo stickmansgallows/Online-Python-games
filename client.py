615fbc3c6285397c8fcd4b5b89bfebd5e03b87df
Bret <stickman.inthe.gallows@gmail.com>
2012-01-27 21:15:18 +0000

    beefed it up server com a bit

--- client.py
+++ client.py
@@ -1,19 +1,13 @@
 #!/usr/apps/bin/python
-#############################################################
-# Client side: use sockets to send data to the server, and 
-# print server's reply to each message line; 'localhost' 
-# means that the server is running on the same machine as 
-# the client, which lets us test client and server on one 
-# machine;  to test over the net, run server on a remote 
-# machine, set serverHost to machine's domain name or IP addr;
-#############################################################
  
 import sys
 from socket import *              # portable socket interface plus constants
 from Tkinter import *
+import time
 
-#serverHost = 'localhost'          # server name, or: 'starship.python.net'
-#serverPort = 50007                # non-reserved port used by the server
+serverHost = 'localhost'          # server name, or: 'starship.python.net'
+serverPort = 50007               # non-reserved port used by the server
+pid = '00'
 
 class MessWin():
     def __init__(self, message, master):
@@ -26,38 +20,29 @@ class ConWin():
 		self.window = Toplevel(master)
 		self.master = master
 		self.window.title("Connect to Server")
-		self.ladd='localhost'
-		self.lport=50007
+		self.ladd=serverHost
+		self.lport=serverPort
 		self.serverHost = Entry(self.window, textvariable=self.ladd)
 		self.serverPort = Entry(self.window, textvariable=self.lport)
-		self.serverHost.insert(0,'localhost')
-		self.serverPort.insert(0,50007)
+		self.serverHost.insert(0,serverHost)
+		self.serverPort.insert(0,serverPort)
 		self.serverHost.pack()
 		self.serverPort.pack()
 		Button(self.window, text="Connect", command=self.connect).pack()
 		
 	def connect(self):
-		self.ladd = self.serverHost.get()
-		self.lport = int(self.serverPort.get())
-		message = ['Hello network world']           # text to send to server
-		sockobj = socket(AF_INET, SOCK_STREAM)      # make a TCP/IP socket object
-		sockobj.connect((self.ladd, self.lport))   # connect to serve and port
-		for line in message:
-		    sockobj.send(line)                      # send line to server over socket
-		    data = sockobj.recv(1024)               # receive from server: up to 1k
-		
-		    #print 'Client received:', `data`
- 		
-		
-		#MessWin(data,self.master)
-		sockobj.close()                             # close to send eof to server
+		global serverHost 
+		serverHost = self.serverHost.get()
+		global serverPort 
+		serverPort = int(self.serverPort.get())
+		global pid
+		message="cStickman"
+		pid = sendReceive(message)
 		self.window.destroy()
-		LobbyWin(self.master, self.ladd,self.lport)
+		LobbyWin(self.master)
 		
 class LobbyWin():
-	def __init__(self, master,ladd,lport):
-		self.sadd=ladd
-		self.sport=lport
+	def __init__(self, master):
 		self.window = Toplevel(master)
 		self.master = master
 		self.chat = Text(self.window, height=40, width=80)
@@ -66,20 +51,24 @@ class LobbyWin():
 		self.enter.pack(side=LEFT)
 		self.send = Button(self.window,text="Send",command=self.sendWords).pack(side=RIGHT)
 		
-		message = ['This is a beautiful day']
-		for i in range(10):
-			sockobj = socket(AF_INET, SOCK_STREAM)      # make a TCP/IP socket object
-			sockobj.connect((self.sadd, self.sport))   # connect to serve and port
-			for line in message:
-		    		sockobj.send(line)                      # send line to server over socket
-		    		data = sockobj.recv(1024)               # receive from server: up to 1k
-				self.chat.insert(END, data+"\n")
-				self.chat.update()
-			sockobj.close()
+		for i in range(20):
+			message = 'p'+pid
+			print sendReceive(message)
+		
 		sys.exit(0)
 	def sendWords(self):
 		print self.enter.get()
 		self.enter.delete(0,END)
+		
+def sendReceive(message):
+	sockobj = socket(AF_INET, SOCK_STREAM)      # make a TCP/IP socket object
+	sockobj.connect((serverHost, serverPort))   # connect to serve and port
+	sockobj.send(message)                      # send line to server over socket
+	data = sockobj.recv(1024)               # receive from server: up to 1k
+	sockobj.close()
+	print "Send: "+message+"   Receive: "+data
+	return data
+	
 
 if __name__ == "__main__":	#Main
     root = Tk()
--- server.py
+++ server.py
@@ -1,22 +1,98 @@
 #!/usr/apps/bin/python
+import asyncore
+import socket
+import time
 
-import SocketServer, time               # get socket server, handler objects
-myHost = ''                             # server machine, '' means local host
-myPort = 50007                          # listen on a non-reserved port number
-def now(): 
-    return time.ctime(time.time())
- 
-class MyClientHandler(SocketServer.BaseRequestHandler):
-    def handle(self):                           # on each client connect
-        print self.client_address, now()        # show this client's addr
-        time.sleep(4)                           # simulate blocking activity
-        while 1:                                # self.request is client socket
-            data = self.request.recv(1024)      # read, write a client socket
-            if not data: break
-            self.request.send('Echo=>%s at %s' % (data, now()))
-        self.request.close() 
- 
-# make a threaded server, listen/handle clients forever
-myaddr = (myHost, myPort)
-server = SocketServer.ThreadingTCPServer(myaddr, MyClientHandler)
-server.serve_forever() 
+serverHost = 'localhost'
+serverPort = 50007
+
+"""
+Code Table:
+All received data will begin with a letter to determine the use
+2nd and 3rd characters will be the playerid given to the client at first log in
+p: ping; 'p##'
+c: connect 'cNAME'
+d: disconnect 'd##'
+r: reconnect 'rNAME'
+s: say 's##STUFF'
+"%02d"%num
+"""
+
+class EchoHandler(asyncore.dispatcher_with_send):
+
+    def handle_read(self):
+        data = self.recv(1024)
+        if data:
+    	if data[0] == 'p':
+            		self.send(str(masterlist.unloadBuffer(int(data[1:3]))))
+			masterlist.checkin(int(data[1:3]))
+		elif data[0] == 'c':
+			self.send("%02d" % masterlist.addPlayer(data[1:])) #Be sure to pad the zeroes
+		elif data[0] == 'd':
+			masterlist.diePlayer(int(data[1:3]))
+		elif data[0] == 's':
+			self.send(masterlist.sayPlayer(int(data[3:])))
+
+class EchoServer(asyncore.dispatcher):
+	def __init__(self, host, port):
+		asyncore.dispatcher.__init__(self)
+		self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.set_reuse_addr()
+		self.bind((host, port))
+		self.listen(5)
+
+	def handle_accept(self):
+        	pair = self.accept()
+        	if pair is None:
+        		pass
+        	else:
+			sock, addr = pair
+			print 'Incoming connection from %s' % repr(addr)
+			handler = EchoHandler(sock)
+
+
+class Playlist():
+	def __init__(self):
+		self.name = []
+		#self.address = []
+		self.pingtime = []
+		self.active = []
+		self.butter = []
+	def addPlayer(self,name):
+		try:
+			i = self.name.index(name):
+		except ValueError:			
+			self.name.append(name)
+			#self.address.append(address)
+			self.pingtime.append(time.ctime(time.time()))
+			self.butter.append([])
+			self.active.append(True)
+			return len(self.name)-1
+		else:
+			return i
+	def diePlayer(self,i):
+		self.active[i]=False
+		#i = self.address.index(address)
+		#self.name.pop(i)
+		#self.address.pop(i)
+		#self.pingtime.pop(i)
+	def checkin(self,i):
+		self.pingtime[i]=time.ctime(time.time())
+	def checkTime(self):
+		t = time.ctime(time.time())
+		for i in range(len(self.name)):
+			if t - self.pingtime[i] > 10: #Obviously needs real maths here
+				self.diePlayer(i)
+	def sayPlayer(self,m):
+		for j in range(len(self.name)):
+			self.butter[j].append(m)
+	def unloadBuffer(self,i):
+		if len(self.butter[i]) > 0:
+			return self.butter[i].pop(0)
+		else:
+			return ''
+
+if __name__ == '__main__':
+	masterlist = Playlist()
+	server = EchoServer(serverHost, serverPort)
+	asyncore.loop()
