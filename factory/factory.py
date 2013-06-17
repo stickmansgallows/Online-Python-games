ec730e51a193017395c945e7d1c20d93acd054f7
stickmansgallows <stickman.inthe.gallows@gmail.com>
2013-06-17 00:26:17 -0600

    Added notes and preliminary files for Factory Manager

---
+++ factory/factory.py
@@ -0,0 +1,15 @@
+import database
+
+class Part:
+	def __init__(self, partType, level):
+		self.partType = partType
+		self.level = level
+		self.used = false
+		self.maxLevel
+		self.params
+		this.cost = cost
+		this.worker = worker
+		this.box = box
+		this.pallet = pallet
+		this.power = power
+
