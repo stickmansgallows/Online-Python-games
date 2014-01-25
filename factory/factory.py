adae3ec73bd2d3d858c08bc3648409cb118196e8
stickmansgallows <stickman.inthe.gallows@gmail.com>
2014-01-24 17:21:36 -0700

    More changes

--- factory/factory.py
+++ factory/factory.py
@@ -6,10 +6,38 @@ class Part:
 		self.level = level
 		self.used = false
 		self.maxLevel
-		self.params
-		this.cost = cost
-		this.worker = worker
-		this.box = box
-		this.pallet = pallet
-		this.power = power
+		for i in attribList:
+			self.attrib[i] = a[partType][i][level]
+		
+	def buy(self,player):
+		self.owner = player
+
+class Player:
+    def __init__(self, number):
+        self.player = number
+        self.slots = 8
+        self.money = 20
+        self.stats = []
+        for attrib in attribList:
+            self.stats[attrib] = 0
+        self.equipment = []
+        self.equipment.append(Part('machine',0))
+        self.equipment.append(Part('machine',0))
+        self.equipment.append(Part('prodRob',0))
+        self.equipment.append(Part('prodRob',0))
+        self.updateState()
+
+    def updateState(self)
+        for attrib in attribList:
+            self.stats[attrib] = 0
+            for parts in self.equipment:
+                self.power += parts[attrib]
+                
+if __name__ == '__main__':
+	players = 5
+	for i in partList:
+		for j in range(len(a[i]['cost']):
+			for k in range(b[i][j][players]):	       
+			       parts.append(Part(i,j))
+	
 
